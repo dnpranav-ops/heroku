d9a0f4df1ee8833333763d49a909609659993df3
Glitch (glitch-hello-node) <none>
2021-11-10 12:10:51 +0000

    🍧🚙 Checkpoint ./app.js:38300582/5204

--- app.js
+++ app.js
@@ -1,53 +1,49 @@
-// Require the Bolt package (github.com/slackapi/bolt)
-const { App } = require("@slack/bolt");
+const { App } = require('@slack/bolt');
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  socketMode: true,
+  appToken: process.env.SLACK_APP_TOKEN,
+  // Socket Mode doesn't listen on a port, but in case you want your app to respond to OAuth,
+  // you still need to listen on some port!
+  port: process.env.PORT || 3000
 });
 
-
-// Message listener function called for messages containing "hello"
+// Listens to incoming messages that contain "hello"
 app.message('hello', async ({ message, say }) => {
-    await say({
-        "blocks": [
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": `👋 Hey there <@${message.user}>`
-                },
-                "accessory": {
-                    "type": "button",
-                    "text": {
-                        "type": "plain_text",
-                        "text": "Click Me",
-                        "emoji": true
-                    },
-                    "action_id": "click_me_button"
-                }
-            }
-        ]
-    });
+  // say() sends a message to the channel where the event was triggered
+  await say({
+    blocks: [
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": `Hey there <@${message.user}>!`
+        },
+        "accessory": {
+          "type": "button",
+          "text": {
+            "type": "plain_text",
+            "text": "Click Me"
+          },
+          "action_id": "button_click"
+        }
+      }
+    ],
+    text: `Hey there <@${message.user}>!`
+  });
 });
 
-// Action listener function called when an interactive component with action_id of “click_me_button” is triggered
-app.action('click_me_button', async ({ ack, body, client, say }) => {
-    // Acknowledge action request before anything else
-    await ack();
-    let channelID = body.channel.id
-    let userID = body.user.id
-    // Respond to action with an ephemeral message
-    await client.chat.postEphemeral({
-    channel: channelID,
-    user: userID,
-    text: `<@${userID}> clicked the button! 🎉 `
-  });
+app.action('button_click', async ({ body, ack, say }) => {
+  // Acknowledge the action
+  await ack();
+  await say(`<@${body.user.id}> clicked the button`);
 });
 
 (async () => {
   // Start your app
-  await app.start(process.env.PORT || 3000);
+  await app.start();
 
   console.log('⚡️ Bolt app is running!');
 })();
